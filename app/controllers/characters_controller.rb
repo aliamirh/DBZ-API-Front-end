7a1665d08bfccbc25401df8fd2fe00db8feac44f
Student <example@example.com>
2020-01-29 10:04:52 -0800

    create basic front end

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,28 @@
+class CharactersController < ApplicationController
+  before_action :set_character, only: [:show, :edit, :update, :destroy]
+
+  # GET /characters
+  # GET /characters.json
+  def index
+    @characters = HTTParty.get('http://localhost:3000/characters')
+
+    :index
+  end
+
+  # GET /characters/1
+  # GET /characters/1.json
+  def show
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_character
+      @character = Character.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def character_params
+      params.fetch(:character, {})
+    end
+end
