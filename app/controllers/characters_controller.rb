ee7b327f2e0d55b7c1516e513031c8eb7f025b89
Student <example@example.com>
2020-01-30 11:03:26 -0800

    change styling

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,25 +3,26 @@ class CharactersController < ApplicationController
   # GET /characters
   # GET /characters.json
   def index
-    @characters = HTTParty.get('http://localhost:3000/characters')
-
-    :index
-  end
-
-  # GET /characters/1
-  # GET /characters/1.json
-  def show
-    @character = HTTParty.get('http://localhost:3000/characters/'+params[:id])
-    # binding.pry
-    :show
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def character_params
-      params.fetch(:character, {})
+    @characters = HTTParty.get("http://localhost:3000/characters/?name=#{params[:name]}", {
+      headers: {"Authorization" => "Bearer secret"}})
+      :index
     end
-end
+
+    # GET /characters/1
+    # GET /characters/1.json
+    def show
+      @character = HTTParty.get('http://localhost:3000/characters/'+params[:id], {
+        headers: {"Authorization" => "Bearer secret"}})
+        # binding.pry
+        :show
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def character_params
+        params.fetch(:character, {})
+      end
+    end
